c2a884d6986cfa85103d6bbcb696e6cc1e63826c
furqan <furqan.jamil.chaud-ry@student.ehb.be>
2023-01-21 17:52:29 +0100

    Added Login

--- app/src/main/java/be/ehb/pokemaper/LoginActivity.java
+++ app/src/main/java/be/ehb/pokemaper/LoginActivity.java
@@ -1,14 +1,86 @@
 package be.ehb.pokemaper;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        if(mAuth.getCurrentUser() != null){
+            finish();
+            return;
+        }
+
+        Button btn_login = findViewById(R.id.btnLogin);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                authenticateUser();
+            }
+        });
+
+        TextView tv_switch_to_register = findViewById(R.id.tv_SwitchToRegister);
+        tv_switch_to_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchToRegister();
+            }
+        });
+    }
+
+    private void authenticateUser(){
+        EditText etLogin_email = findViewById(R.id.et_LoginEmail);
+        EditText etLogin_password = findViewById(R.id.et_LoginPassword);
+
+        String email = etLogin_email.getText().toString();
+        String password = etLogin_password.getText().toString();
+
+        if(email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this, "Please fill in the form", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                           showMainActivity();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Authentication failed", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+    private void showMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void switchToRegister(){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
